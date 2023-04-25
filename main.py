0f396cd00d8628638e2a7be53cf0785468523d73
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-04-25 11:38:34 +0200

    Add key bind event

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from type_test import TypeTest
 from tkinter import *
 from tkinter import messagebox
+import time
+
 
 typetest = TypeTest()
+
 window = Tk()
 window.title("Typing Speed Test")
 window.config(padx=50, pady=50)
 
 messagebox.showinfo(
-    message="This is a one minute typing test to meassure your typing speed. When ready dismiss this message and start typing the words that appear in the window.")
+    message="This is a one paragraph typing test to meassure your typing speed. When ready dismiss this message and start typing the words that appear in the window.")
 
 
 # Labels
@@ -19,8 +22,9 @@ typing_label = Label(text="Type here: ")
 typing_label.grid(column=0, row=1)
 
 # Inputs
-text_input = Entry(width=20)
+text_input = Entry(width=60)
 text_input.grid(column=0, row=2)
 text_input.focus()
+text_input.bind("<KeyPress>", print)
 
 window.mainloop()
